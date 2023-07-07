b387b094abfe099b0ce10b3dc07e147169ac9ad5
amashali <desilvanas.19@uom.lk>
2023-07-07 14:10:22 +0530

    env changes

--- server/database/db.js
+++ server/database/db.js
@@ -10,8 +10,8 @@ const mysql = require("mysql");
 const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_NAME || "janajaya",
+  password: process.env.DB_PASSWORD || "by5x23mw",
+  database: process.env.DB_NAME || "randcopf_janajaya",
   port: process.env.DB_PORT || 3306,
 });
 
