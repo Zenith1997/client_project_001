10f003064afff60965c0f934f5fab075d280c01a
Zenith1997 <dabarerawza.19@uom.lk>
2024-10-16 10:52:56 +0530

    changes

--- server/database/db.js
+++ server/database/db.js
@@ -1,19 +1,18 @@
-const mysql = require("mysql");
-require('dotenv').config();
-// const db = mysql.createConnection({
-//   host: process.env.DB_HOST || "localhost",
-//   user: process.env.DB_USER || "randcopf_zenith",
-//   password: process.env.DB_PASSWORD || "Zenith123",
-//   database: process.env.DB_NAME || "randcopf_janajaya",
-//   port: process.env.DB_PORT || 3306,
-// });
+const mysql = require('mysql2');require('dotenv').config();
 const db = mysql.createConnection({
-  host: process.env.DB_HOST ,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME ,
-  port: process.env.DB_PORT ,
+  host:"localhost",
+  user:"root",
+  password:"root",
+  database:"janajaya",
+  port:3306,
 });
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST ,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME ,
+//   port: process.env.DB_PORT ,
+// });
 
 db.connect((err) => {
   if (err) {
